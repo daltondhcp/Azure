eba95e15ee62a1edcc0ca9a10943b59cac7db089
Johan Dahlbom <johan@dahlbom.eu>
2020-02-23 20:13:50 +0100

    Update DemoConfig.ps1

--- DemoConfig.ps1
+++ DemoConfig.ps1
@@ -1,5 +1,5 @@
 Configuration DemoConfig {
-Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
+Import-DscResource -ModuleName 'PSDscResources'
 
     WindowsFeature WebServer {
         Ensure  = 'Absent'
@@ -12,27 +12,27 @@ Import-DscResource -ModuleName 'PSDesiredStateConfiguration'
                 GetScript = $GetScript
                 SetScript = $SetScript
                 TestScript = $TestScript
-                Result = ('True' -in (Test-Path d:\installpbi64.msi))
+                Result = ('True' -in (Test-Path d:\depagent.exe))
             }
         }
         SetScript = 
         {
             #Download files to temporary storage
-            Invoke-WebRequest -Uri "https://download.microsoft.com/download/9/B/A/9BAEFFEF-1A68-4102-8CDF-5D28BFFE6A61/PBIDesktop_x64.msi" -OutFile "D:\installpbi64.msi"
+             Invoke-WebRequest https://aka.ms/dependencyagentwindows -OutFile d:\depagent.exe
         }
 
         TestScript = 
         {
-            $Status = ('True' -in (Test-Path d:\installpbi64.msi))
+            $Status = ('True' -in (Test-Path d:\depagent.exe))
             $Status -eq $True
         }
     }
-    Package InstallPbi64 
-    {
-        Ensure = 'Present'
-        Name = 'Microsoft Power BI Desktop (x64)'
-        Path = 'd:\installpbi64.msi'
-        Arguments = '/qn /norestart ACCEPT_EULA=1'
-        ProductId = 'A1B8A2F7-C948-47FB-AD92-B4AF7BEF402F'
-    }
+    Package InstallDepAgent {
+            Ensure = 'Present'
+            Name = 'Dependency Agent'
+            Path = d:\depagent.exe
+            Arguments = '/S /AcceptEndUserLicenseAgreement:1'
+            ProductId = ''
+            DependsOn = '[Package]InstallMMAAgent'
+   }
 }
